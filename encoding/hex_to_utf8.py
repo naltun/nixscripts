b755aeacc8735ec9c7ca45d21bd67e7e25966094
Noah Altunian <nbaltunian@gmail.com>
2020-04-09 19:29:14 +0000

    Refactor hex to UTF8

--- encoding/hex_to_utf8.py
+++ encoding/hex_to_utf8.py
@@ -1,6 +1,17 @@
 #!/usr/bin/env python3
 import sys
 
-if len(sys.argv) != 2:
-    print("No argument supplied."); exit(1) 
-print(f"UTF-8 encoding: {bytearray.fromhex(sys.argv[1]).decode('UTF-8')}")
+
+def decode_hex_to_ut8(data):
+    try:
+        return bytearray.fromhex(data).decode("UTF-8")
+    except ValueError:
+        return "Error when parsing hex string -- Check that it's valid."
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("No argument supplied."); exit(1)
+    else:
+        print(decode_hex_to_ut8(sys.argv[1]))
+
