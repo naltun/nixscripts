80b4ab6a82142824c6aea89235524c3f8aa066bb
Noah Altunian <nbaltunian@gmail.com>
2020-04-09 13:16:02 +0000

    Add __main__; Refactor

--- ciphers/rot13.py
+++ ciphers/rot13.py
@@ -2,9 +2,15 @@
 import codecs
 import sys
 
-if len(sys.argv) != 2:
-    print("No argument supplied."); exit(1)
-else:
-    print(codecs.encode(sys.argv[1], "rot13"))
-    exit(0)
+
+def apply_rot13(data):
+    return codecs.encode(data, "rot13")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("No argument supplied."); exit(1)
+    else:
+        print(apply_rot13(sys.argv[1]))
+        exit(0)
 
